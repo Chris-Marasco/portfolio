a4740029c95887ece8d4f3d765ebffa53b494210
Chris Marasco <cxm6467@gmail.com>
2022-09-03 23:44:01 -0400

    cicd

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect"
+    '@testing-library/jest-dom/extend-expect'
   ]
-};
+}
